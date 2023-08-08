d4565a7815b8a2c2c9189c7ffcb28140094dbdf8
Margareth Dias <mdias@c3r4p2.42sp.org.br>
2023-08-08 20:51:53 -0300

    finish mandatory part

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mdias <mdias@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 17:54:29 by mdias             #+#    #+#             */
-/*   Updated: 2023/08/03 21:08:41 by mdias            ###   ########.fr       */
+/*   Updated: 2023/08/08 20:42:12 by mdias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,5 +46,10 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
